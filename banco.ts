72aa8083a317c51d1fb7364f63ec12a1f6fa0351
STEVAO SEVERO RODRIGUES <04460096013@senacrs.edu.br>
2024-11-11 21:36:31 -0300

    23 MODIFICAÇAO
    23 COMMIT

--- banco.ts
+++ banco.ts
@@ -99,3 +99,55 @@ function menuInicial(): void {
     }
 }// Commit 22: Continuando a execução do código com o menu inicial
 menuInicial();
+
+// Commit 23: Melhorando o menu bancário e opções de conta
+function menuBancario(): void {
+    console.clear();
+    let continuar = true;
+    while (continuar) {
+        console.log(`\
+        ------------------------------------
+        ----------- MENU BANRISUL ----------
+        ------------------------------------
+        - 1. CRIAR NOVA CONTA              -
+        - 2. ENTRAR NA CONTA               -
+        - 3. VOLTAR AO MENU INICIAL        -
+        ------------------------------------`);
+
+        let opcao = rl.questionInt("Escolha uma opcao: ");
+
+        switch (opcao) {
+            case 1:
+                console.clear();
+                let tipo = rl
+                    .question(` 
+        -------------------------------------------------
+        ----------- ESCOLHA SUA OPCAO DE CONTA ----------
+        -------------------------------------------------
+                        1 - POUPANCA
+                        2 - CORRENTE 
+                        `)
+                    .toLowerCase();
+                    if (tipo === "1" || tipo === "2") {
+                    let novaConta = criarConta(tipo);
+                    contas.push(novaConta);
+                    console.clear();
+                    }
+                break;
+            case 2:
+                console.clear();
+                user = EntrarNaConta(); 
+                if (user) {
+                    menuConta(user);
+                }
+                break;
+            case 3:
+                console.clear();
+                console.log("Voltando ao menu inicial...");
+                continuar = false;
+                break;
+            default:
+                console.log("Opção inválida. Tente novamente.");
+        }
+    }
+}
