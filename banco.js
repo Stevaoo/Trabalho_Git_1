32391ff6fc080f4424804a3f1fab919becb1b520
MATHEUS BRAGA MATTOS <04968890010@senacrs.edu.br>
2024-11-11 21:26:41 -0300

    5 modificacao
    5 commit

--- banco.js
+++ banco.js
@@ -28,3 +28,91 @@ function criarConta(tipo: string): ContaBancaria {
         throw new Error("Tipo de conta inválido.");
     }
 }
+
+// Commit 5: Função FAQ para tratar dúvidas frequentes dos usuários
+function FAQ(): void {
+    console.clear();
+    console.log(`\ 
+        ---------------------------------------------------------------------------------------------
+        ------------------------------ PERGUNTAS FREQUENTES -----------------------------------------
+        ---------------------------------------------------------------------------------------------
+        - 1. COMO FAÇO LOGIN NA MINHA CONTA BANCÁRIA ONLINE ?                                       -
+        - 2. ESQUECI MINHA SENHA. COMO POSSO RECUPERA-LÁ ?                                          -
+        - 3. O QUE FAZER SE MINHA CONTA FOR BLOQUEADA OU SUSPENSA ?                                 -
+        - 4. COMO POSSO VERIFICAR O SALDO E HISTÓRICO DE TRANSAÇÕES DA MINHA CONTA ?                -
+        - 5. COMO REALIZAR TRANSFERÊNCIAS ENTRE CONTAS OU PARA TERCEIROS PELO SITE ?                -
+        - 6. O QUE FAZER SE EU SUSPEITAR DE ATIVIDADE FRAUDELENTA NA MINHA CONTA ?                  -
+        ---------------------------------------------------------------------------------------------
+        ---------------------------- DIGITE "SAIR" PARA VOLTAR --------------------------------------
+        ---------------------------------------------------------------------------------------------`);
+
+    let loop = true;
+
+    while (loop) {
+        let duvida = rl.question("Digite o numero da sua duvida ou 'SAIR' para voltar: ");
+        switch (duvida) {
+            case "1":
+            case " COMO FAÇO LOGIN NA MINHA CONTA BANCÁRIA ONLINE ?":
+            console.clear();
+                console.log(`\ 
+                1 - ➱ Quando estiver no menu inicial do sistema, escolha a opção para "Entrar na Conta".
+                2 - ✔ Você será solicitado a inserir o ID da sua conta e a senha correspondente.
+                3 - ✔ O sistema verifica se o ID e a senha inseridos correspondem a uma conta existente. 
+                Se estiverem corretos, você será autenticado e poderá acessar as funcionalidades da sua conta.
+                4 - ✔ Se o login for bem-sucedido, uma mensagem de boas-vindas com o seu nome será exibida, 
+                indicando que você entrou na conta com sucesso.\n`);
+                break;
+            case "2":
+            case "ESQUECI MINHA SENHA. COMO POSSO RECUPERA-LÁ ?":
+            console.clear();
+                console.log(`Sinto muito, nosso Banco é inicial....seu dinheiro agora é nosso! Obrigado.`);
+                break;
+
+            case "3":
+            case "O QUE FAZER SE MINHA CONTA FOR BLOQUEADA OU SUSPENSA ?":
+            console.clear();
+                console.log("Se for chorar, manda áudio! Nós não nos responsabilizamos por contas bloqueadas ou suspensas.\n");
+                break;
+
+            case "4":
+            case " COMO POSSO VERIFICAR O SALDO E HISTÓRICO DE TRANSAÇÕES DA MINHA CONTA ?":
+            console.clear();
+                console.log(`\ 
+                    1 - ➱ No menu bancário, selecione a opção 2. Entrar na Conta.
+                    2 - ✔ Insira o ID da conta e a senha.
+                    3 - ✔ No menu da conta, selecione a opção 1. 
+                    Ver Extrato para visualizar o saldo e o histórico de transações.\n`);
+                break;
+
+            case "5":
+            case " COMO REALIZAR TRANSFERÊNCIAS ENTRE CONTAS OU PARA TERCEIROS PELO SITE ?":
+            console.clear();
+                console.log(`\ 
+                    1 - ➱ No menu bancário, selecione a opção 2. Entrar na Conta.
+                    2 - ✔ Insira o ID da conta e a senha.
+                    3 - ✔ No menu da conta, selecione a opção 4. Transferência.
+                    4 - ✔ Insira o valor da transferência.
+                    5 - ✔ Insira o ID da conta de destino. (O sistema verificará se a conta de destino existe.)
+                    6 - ✔ O sistema realizará a transferência e atualizará o seu saldo e o histórico de transações.\n`);
+                break;
+
+            case "6":
+            case "O QUE FAZER SE EU SUSPEITAR DE ATIVIDADE FRAUDELENTA NA MINHA CONTA ?":
+            console.clear();
+                console.log(`Ligue para a central de atendimento do banco pelo número 4002-8922. 
+                    \n Informe o problema e forneça os detalhes necessários para que a equipe possa investigar a situação.\n`);
+                break;
+
+            case "SAIR":
+            case "Sair":
+            case "sair":
+                loop = false;
+                console.log("Saindo do FAQ...");
+                break;
+
+            default:
+                console.log("Opção inválida. Tente novamente.");
+                break;
+        }
+    }
+}
