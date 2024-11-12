e2fd442d9312623d1dc319bdf5744fe0cfe5cc82
STEVAO SEVERO RODRIGUES <04460096013@senacrs.edu.br>
2024-11-11 21:25:34 -0300

    16 Commit
    vamoooooooooooo

---
+++ banco.ts
@@ -0,0 +1,7 @@
+// Commit 15: Função para pressionar Enter para continuar
+function pressionarEnterParaContinuar(): void {
+    rl.question("Pressione Enter para continuar...");
+}
+// Commit 17: Chamada para iniciar o sistema
+menuInicial();
+//
